1b5d2c1838e1b786c48e1466f9aa1e99a8429ce2
Matteo <mcavalluzzo.dev@gmail.com>
2020-05-12 19:09:34 +0200

    remove test component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.scss';
-import HelloUser from "./components/hellouser.component";
-import {Switch, Route} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import SignIn from "./components/signin/signin.component";
 import MenuAppBar from "./components/appbar/menuappbar.component";
 import Categories from "./pages/catalog/catalogpage.component";
 
+
 function App() {
     return (
-        <div className="App">
-            <HelloUser/>
-            <MenuAppBar />
+        <div>
+            <MenuAppBar/>
             <Switch>
                 <Route exact path="/signin" component={SignIn}/>
-                <Route path='/' component={Categories} />
+                <Route path='/' component={Categories}/>
             </Switch>
         </div>
     );
