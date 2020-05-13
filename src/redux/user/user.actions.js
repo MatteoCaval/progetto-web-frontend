88d2fcfb29ba86a1f401c1903fb9c999169f1a7c
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-05-13 18:07:29 +0200

    prepared registration actions

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import AuthActionType from "../auth/auth.actionType";
+import Config from '../../config'
 
 export const UserActionTypes = {
     SET_USER: 'SET_USER'
@@ -28,15 +29,39 @@ export const loginFailed = errorMessage => {
     }
 }
 
+export const registrationSuccess = user => {
+    return {
+        type: AuthActionType.REGISTRATION_SUCCESS,
+        payload: user
+    }
+}
+
+export const registrationFailed = errorMessage => {
+    return {
+        type: AuthActionType.REGISTRATION_FAILED,
+        payload: errorMessage
+    }
+}
+
 
 // TODO create auth package and move inside it
 export const loginUser = (email, password) => {
     return dispatch => {
-        axios.post('http://localhost:3001/auth/signin ', { email, password })
+        axios.post(`${Config.API_BASE_URL}/auth/signin`, { email, password })
             .then(result => {
                 dispatch(loginSuccess(result.data))
             })
             .catch(error => dispatch(loginFailed(error)))
 
+    }
+}
+
+export const registerUser = (user) => {
+    return dispatch => {
+        axios.post(`${Config.API_BASE_URL}/auth/signup`, user)
+            .then(result => {
+                dispatch(registrationSuccess(result.data))
+            })
+            .catch(error => dispatch(registrationFailed(error)))
     }
 }
