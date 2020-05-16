aee5721d64efa8a3f8ca11c301806bd60acc2a64
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-05-16 19:08:30 +0200

    compete order connected

--- src/components/cart/cartpage.component.js
+++ src/components/cart/cartpage.component.js
@@ -4,13 +4,12 @@ import { connect } from "react-redux";
 import CartProductItem from "./cartproductitem.component";
 import { Grid, Container, Divider, Button, Typography } from "@material-ui/core";
 import "./cartpage.style.scss"
+import { completeOrder } from "../../redux/orders/orders.actions";
 
-const CartPage = ({ fetchCart, cart }) => {
+const CartPage = ({ fetchCart, cart, completeOrder }) => {
     useEffect(() => {
         fetchCart()
     }, [fetchCart])
-
-    console.log(cart.products)
 
     return (
         <Container maxWidth='md'>
@@ -19,27 +18,25 @@ const CartPage = ({ fetchCart, cart }) => {
                     cart.products.map(product => {
                         return (
                             <Grid key={product.id} item xs={12} sm={12}>
-                                <CartProductItem key={product.id} product={product} />
+                                <CartProductItem key={product.id} product={product}/>
                             </Grid>
                         )
                     })
                 }
             </Grid>
-            <Divider className='cart-total-divider' />
+            <Divider className='cart-total-divider'/>
             <div className="total-container">
                 <Button
                     variant="contained"
                     color="primary"
-                    className="proceed-to-order">
+                    className="proceed-to-order"
+                    onClick={() => completeOrder()}
+                >
                     Proceed to order
-                        </Button>
+                </Button>
                 <div className="total-info-container">
-                    <Typography variant='h6' color='textPrimary'>
-                        Total:
-                            </Typography>
-                    <Typography variant='h5' color='textPrimary'>
-                        1000.00€
-                            </Typography>
+                    <Typography variant='h6' color='textPrimary'>Total:</Typography>
+                    <Typography variant='h5' color='textPrimary'>1000.00€</Typography>
                 </div>
             </div>
         </Container>
@@ -54,7 +51,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        fetchCart: () => dispatch(fetchCart())
+        fetchCart: () => dispatch(fetchCart()),
+        completeOrder: () => dispatch(completeOrder())
     }
 }
 
