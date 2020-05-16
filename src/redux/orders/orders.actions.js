5b115961e122179f3d8d949eb7f7f876a7e692f4
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-05-16 18:34:32 +0200

    complete order actions in progress

---
+++ src/redux/orders/orders.actions.js
@@ -0,0 +1,39 @@
+import OrderActionTypes from "./orders.types";
+import axios from 'axios'
+import Config from "../../config";
+
+export const completeOrder = (/*oder data? */) => {
+    return (dispatch, getState) => {
+        dispatch(completeOrderPending())
+        const token = getState().user.currentUser.token
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+        axios.post(`${Config.API_BASE_URL}/orders`)
+            .then(result => {
+                dispatch(completeOrderSuccess())
+            })
+            .catch(error => dispatch(completeOrderFailed(error)))
+
+    }
+}
+
+export const completeOrderSuccess = () => {
+    return {
+        type: OrderActionTypes.COMPLETE_ORDER_SUCCESS
+    }
+}
+
+export const completeOrderFailed = (error) => {
+    return {
+        type: OrderActionTypes.COMPLETE_ORDER_FAILED,
+        payload: error.message
+    }
+}
+
+export const completeOrderPending = () => {
+    return {
+        type: OrderActionTypes.COMPLETE_ORDER_PENDING
+    }
+}
+
+
+
