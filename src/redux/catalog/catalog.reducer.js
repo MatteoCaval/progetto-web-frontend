723ae6db1caa37787bb6e084155bdef687e111a3
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-05-21 00:09:48 +0200

    catalog loading in progress

--- src/redux/catalog/catalog.reducer.js
+++ src/redux/catalog/catalog.reducer.js
@@ -3,7 +3,9 @@ import CatalogActionType from "./catalog.types";
 const INITIAL_STATE = {
     categories: [],
     products: [],
-    productDetails: null
+    productDetails: null,
+    loading: false,
+    error: ''
 }
 
 const catalogReducer = (state = INITIAL_STATE, action = {}) => {
@@ -11,22 +13,36 @@ const catalogReducer = (state = INITIAL_STATE, action = {}) => {
         case CatalogActionType.FETCH_CATEGORIES_SUCCESS:
             return {
                 ...state,
-                categories: action.payload
+                categories: action.payload,
+                loading: false
+            }
+        case CatalogActionType.FETCH_CATEGORIES_PENDING:
+            return {
+                ...state,
+                loading: true
             }
         case CatalogActionType.FETCH_PRODUCT_SUCCESS:
             return {
                 ...state,
-                productDetails: action.payload
+                productDetails: action.payload,
+                loading: false
             }
         case CatalogActionType.FETCH_PRODUCT_PENDING:
             return {
                 ...state,
-                productDetails: null
+                productDetails: null,
+                loading: true
             }
         case CatalogActionType.FETCH_CATEGORY_PRODUCTS_SUCCESS:
             return {
                 ...state,
-                products: action.payload
+                products: action.payload,
+                loading: false
+            }
+        case CatalogActionType.FETCH_CATEGORY_PRODUCTS_PENDING:
+            return {
+                ...state,
+                loading: true
             }
         default:
             return state
