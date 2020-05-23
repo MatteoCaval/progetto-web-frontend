dc8823fa89446bbf1a046ed07464fa83bfe42b12
Unknown <matteo.cavalluzzo@gmail.com>
2020-05-23 22:11:39 +0200

    attach drawer to riders and timetable page

--- src/components/appbar/menuappbar.component.js
+++ src/components/appbar/menuappbar.component.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import ShoppingCart from '@material-ui/icons/ShoppingCart';
-import { Menu, MenuItem, IconButton, Typography, Toolbar, AppBar, Drawer } from '@material-ui/core';
+import { Menu, MenuItem, IconButton, Typography, Toolbar, AppBar, Drawer, Fab } from '@material-ui/core';
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { logout } from "../../redux/user/user.actions";
@@ -14,7 +14,7 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import ScheduleIcon from '@material-ui/icons/Schedule';
 import DirectionsBikeIcon from '@material-ui/icons/DirectionsBike';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link as RouterLink } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -59,25 +59,29 @@ const MenuAppBar = ({ currentUser, history, logout }) => {
         history.push('/')
     }
 
+    const hideDrawer = () => setDrawerOpen(false)
+
     return (
         <div className={classes.root}>
-            <Drawer anchor='left' open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+            <Drawer anchor='left' open={drawerOpen} onClose={hideDrawer}>
                 <List className={classes.list}>
                     <ListItem button>
                         <ListItemIcon>{<ListAltIcon/>}</ListItemIcon>
                         <ListItemText primary='Orders'/>
                     </ListItem>
-                    <ListItem button>
+                    <ListItem button
+                              component={RouterLink}
+                              to="/timetable"
+                              onClick={hideDrawer}>
                         <ListItemIcon>{<ScheduleIcon/>}</ListItemIcon>
                         <ListItemText primary='Timetable'/>
                     </ListItem>
-                    <ListItem button>
+                    <ListItem button
+                              component={RouterLink}
+                              to='/riders'
+                              onClick={hideDrawer}>
                         <ListItemIcon>{<DirectionsBikeIcon/>}</ListItemIcon>
                         <ListItemText primary='Riders'/>
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemIcon>{<ShoppingBasketIcon/>}</ListItemIcon>
-                        <ListItemText primary='Catalog'/>
                     </ListItem>
                 </List>
             </Drawer>
