719ab5c2d480ab3057d015f443aba21256fbb382
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-05-24 13:12:32 +0200

    change rider card layout

--- src/components/rider/rider-item.component.js
+++ src/components/rider/rider-item.component.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Card, CardContent, Typography } from "@material-ui/core";
+import { Card } from "@material-ui/core";
+import CardHeader from "@material-ui/core/CardHeader";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete"
 
 const RiderItem = ({ rider }) => {
     return (
         <Card>
-            <CardContent>
-                <Typography color='textPrimary'>{`${rider.name} ${rider.surname}`}</Typography>
-                <Typography color='textPrimary'>{rider.email}</Typography>
-            </CardContent>
+            <CardHeader
+
+                action={
+                    <IconButton aria-label="settings">
+                        <DeleteIcon/>
+                    </IconButton>
+                }
+                title={`${rider.name} ${rider.surname}`}
+                subheader={rider.email}
+            />
         </Card>
     )
 }
