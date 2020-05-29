397655ff33f4bad8a45efa15ff6022a65cff7ae6
Unknown <matteo.cavalluzzo@gmail.com>
2020-05-29 15:48:02 +0200

    tabs working

--- src/components/orders/orders-page.component.js
+++ src/components/orders/orders-page.component.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchOrders } from "../../redux/user/user.actions";
 import { Container, Tab, Tabs } from "@material-ui/core";
 import { connect } from "react-redux";
@@ -6,25 +6,37 @@ import OrderList from "./orders-list.component";
 import AppBar from "@material-ui/core/AppBar";
 
 const OrdersPage = ({ orders, fetchOrders }) => {
+
+    const [tabValue, setTabValue] = useState(0)
+
     useEffect(() => {
         fetchOrders()
     }, [fetchOrders])
 
     return (
         <React.Fragment>
-            {/*<AppBar position='static'>*/}
-            {/*    <Tabs*/}
-            {/*        indicatorColor="inherit"*/}
-            {/*        textColor="inherit"*/}
-            {/*        centered*/}
-            {/*    >*/}
-            {/*        <Tab label='working'/>*/}
-            {/*        <Tab label='history'/>*/}
-            {/*    </Tabs>*/}
-            {/*</AppBar>*/}
+            <AppBar position='static'>
+                <Tabs
+                    value={tabValue}
+                    onChange={(e, newValue) => setTabValue(newValue)}
+                    indicatorColor="secondary"
+                    textColor="secondary"
+                    centered
+                >
+                    <Tab label='working'/>
+                    <Tab label='history'/>
+                </Tabs>
+            </AppBar>
 
             <Container maxWidth='md'>
-                <OrderList orders={orders}/>
+                {/*first tab*/}
+                <div hidden={tabValue !== 0}>
+                   <p>Primo tab</p>
+                </div>
+                {/*second tab*/}
+                <div hidden={tabValue !== 1}>
+                    <OrderList orders={orders}/>
+                </div>
             </Container>
         </React.Fragment>
     )
