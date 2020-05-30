b31b98559060b3faee47dbbff4d84fe6b90ebda0
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-05-30 18:04:08 +0200

    tab contraints

--- src/components/orders/orders-page.component.js
+++ src/components/orders/orders-page.component.js
@@ -1,26 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Tab, Tabs } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import PaginatedOrderList from "./paginated-order-list.component";
 import LiveUpdatedOrderList from "./live-updated-order-list.component";
+import { connect } from "react-redux";
+import UserRoles from "../../common/UserRoles";
+import { AdminConstrained } from "../common/constrained-containers.component";
 
-const OrdersPage = () => {
-    const [tabValue, setTabValue] = useState(0)
+const getInitialTabValue = (userRole) => {
+    switch (userRole) {
+        case UserRoles.CONSUMER:
+            return 1
+        case UserRoles.RIDER:
+            return 0
+        default:
+            return 0
+    }
+}
+
+
+const OrdersPage = ({ userRole }) => {
+    const [tabValue, setTabValue] = useState(getInitialTabValue(userRole))
+
 
     return (
         <React.Fragment>
-            <AppBar position='static'>
-                <Tabs
-                    value={tabValue}
-                    onChange={(e, newValue) => setTabValue(newValue)}
-                    indicatorColor="secondary"
-                    textColor="secondary"
-                    centered
-                >
-                    <Tab label='working'/>
-                    <Tab label='history'/>
-                </Tabs>
-            </AppBar>
+            <AdminConstrained>
+                <AppBar position='static'>
+                    <Tabs
+                        value={tabValue}
+                        onChange={(e, newValue) => setTabValue(newValue)}
+                        indicatorColor="secondary"
+                        textColor="secondary"
+                        centered
+                    >
+                        <Tab label='working'/>
+                        <Tab label='history'/>
+                    </Tabs>
+                </AppBar>
+            </AdminConstrained>
 
             <Container maxWidth='md'>
                 {/*first tab*/}
@@ -37,4 +55,11 @@ const OrdersPage = () => {
 }
 
 
-export default OrdersPage
+const mapStateToProps = state => {
+    const userRole = state.user.currentUser ? state.user.currentUser.role : null
+    return {
+        userRole
+    }
+}
+
+export default connect(mapStateToProps)(OrdersPage)
