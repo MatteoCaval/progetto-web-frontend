50fa5460c9b1d10b41f1c45547968e7e1e1742b4
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-06-01 15:00:22 +0200

    socket io connection test working

--- src/components/orders/live-updated-order-list.component.js
+++ src/components/orders/live-updated-order-list.component.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import OrderList from "./orders-list.component";
 import { connect } from "react-redux";
+import Config from "../../config";
+import io from "socket.io-client";
 
 const LiveUpdatedOrderList = ({}) => {
 
+    useEffect(() => {
+        console.log('init io')
+        const client = io.connect(Config.API_BASE_URL)
+        client.on('prova', msg => console.log(msg))
+    }, [])
 
     return (
         <React.Fragment>
