4e8e21e7bbfb3c457edc313584b17cb9c8363a00
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-06-01 17:13:10 +0200

    first test of updating order list working

--- src/components/orders/live-updated-order-list.component.js
+++ src/components/orders/live-updated-order-list.component.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import OrderList from "./orders-list.component";
 import { connect } from "react-redux";
 import Config from "../../config";
@@ -6,16 +6,22 @@ import io from "socket.io-client";
 
 const LiveUpdatedOrderList = ({}) => {
 
+    const [orders, setOrders] = useState([])
+
     useEffect(() => {
         console.log('init io')
         const client = io.connect(Config.API_BASE_URL)
         client.on('prova', msg => console.log(msg))
+        client.on('orders', orders => {
+            console.log(orders)
+            setOrders(orders)
+        })
     }, [])
 
     return (
         <React.Fragment>
             <p>Live updated order list</p>
-            <OrderList orders={[]}/>
+            <OrderList orders={orders}/>
         </React.Fragment>
     )
 
