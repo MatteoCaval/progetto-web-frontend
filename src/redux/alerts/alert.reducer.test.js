198d5bb0bca879c56060af0c5aed7ddcd4416ca4
Unknown <matteo.cavalluzzo@gmail.com>
2020-06-07 08:28:30 +0200

    alert reducer test completed

--- src/redux/alerts/alert.reducer.test.js
+++ src/redux/alerts/alert.reducer.test.js
@@ -1,10 +1,56 @@
 import alertReducer from "./alert.reducer";
+import AlertTypes from "./alert.types";
+import { alertActions } from "./alert.actions";
 
+const initialState = {}
 
 describe('alert reducer', () => {
 
     it('should return initial state', () => {
         expect(alertReducer()).toEqual({})
+    })
+
+    it('should set message after alert success action', () => {
+        const message = 'mockmessage'
+
+        expect(
+            alertReducer(initialState, {
+                type: AlertTypes.SUCCESS,
+                payload: message
+            })
+        ).toEqual({
+            type: AlertTypes.SUCCESS,
+            message: message
+        })
+
+    })
+
+    it('should set message after alert error action', () => {
+        const message = 'mockmessage'
+
+        expect(
+            alertReducer(initialState, {
+                type: AlertTypes.ERROR,
+                payload: message
+            })
+        ).toEqual({
+            type: AlertTypes.ERROR,
+            message: message
+        })
+
+    })
+
+    it('should clear state after clear action', () => {
+        const randomState = {
+            type: AlertTypes.SUCCESS,
+            message: 'test'
+        }
+
+        expect(
+            alertReducer(randomState, alertActions.clear())
+        ).toEqual(
+            {}
+        )
     })
 
 })
