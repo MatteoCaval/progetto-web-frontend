65abb5f7338ece936d3f47441f72fb04ea4c4e7e
matteo cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-11-22 15:07:24 +0100

    Show cart error

--- src/components/cart/cartpage.component.js
+++ src/components/cart/cartpage.component.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from "react";
-import { fetchCart } from "../../redux/cart/cart.actions";
-import { connect } from "react-redux";
+import React, {useEffect} from "react";
+import {fetchCart} from "../../redux/cart/cart.actions";
+import {connect} from "react-redux";
 import CartProductItem from "./cartproductitem.component";
-import { Button, Container, Grid, Typography } from "@material-ui/core";
-import { withRouter } from "react-router-dom";
+import {Button, Container, Grid, Typography} from "@material-ui/core";
+import {withRouter} from "react-router-dom";
 
 import "./cartpage.style.scss"
 import Progress from "../common/progress.component";
 import HorizontalDivider from "../custom/horizontal-divider.component";
 
 const CartPage = ({ fetchCart, cart, history }) => {
+
     useEffect(() => {
         fetchCart()
     }, [fetchCart])
@@ -20,14 +21,14 @@ const CartPage = ({ fetchCart, cart, history }) => {
     if (products && products.length) {
         return (
             <main>
-                <Typography variant="h1" className="page-title">Cart</Typography>      
+                <Typography variant="h1" className="page-title">Cart</Typography>
                 <Container maxWidth='md'>
                     <Grid className="products-list" container spacing={2}>
                         {
                             products.map(product => {
                                 return (
                                     <Grid key={product.id} item xs={12} sm={12}>
-                                        <CartProductItem key={product.id} product={product} />
+                                        <CartProductItem key={product.id} product={product}/>
                                     </Grid>
                                 )
                             })
@@ -35,7 +36,7 @@ const CartPage = ({ fetchCart, cart, history }) => {
                     </Grid>
 
                     <div className="cart-page-end-list">
-                        <HorizontalDivider />
+                        <HorizontalDivider/>
                         <div className="total-container">
                             <Button
                                 variant="contained"
@@ -43,7 +44,7 @@ const CartPage = ({ fetchCart, cart, history }) => {
                                 className="proceed-to-order"
                                 onClick={() => history.push('/summary')}>
                                 Proceed to order
-                        </Button>
+                            </Button>
 
                             <div className="total-info-container">
                                 <Typography variant='h2' color='textPrimary'>Total:</Typography>
@@ -51,33 +52,41 @@ const CartPage = ({ fetchCart, cart, history }) => {
                             </div>
                         </div>
                     </div>
-                    <Progress loading={loading} />
+                    <Progress loading={loading}/>
                 </Container>
             </main>
         )
     } else {
-        return (
-            <Container maxWidth='md'>
-                {(products && !loading && !error) ? (
-                    <div className="empty-shop-button-container">
-                        <Typography variant='h2' color='textPrimary'>
-                            Il carrello è vuoto
-                        </Typography>
-                        <Typography color='textPrimary'>
-                            Al momento all'interno del tuo carrello non sono presenti prodotti.
-                        </Typography>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            className="back-to-shop-button"
-                            onClick={() => history.push('/')}>
-                            Back to shop
-                        </Button>
-                    </div>
-                ) : null
-                }
-            </Container>
-        )
+        if (error && error.description) {
+            return (
+                // TODO visualizzare errore più bello
+                <p>{error.description}</p>
+            )
+        } else {
+            return (
+                <Container maxWidth='md'>
+                    {(products && !loading && !error) ? (
+                        <div className="empty-shop-button-container">
+                            <Typography variant='h2' color='textPrimary'>
+                                Il carrello è vuoto
+                            </Typography>
+                            <Typography color='textPrimary'>
+                                Al momento all'interno del tuo carrello non sono presenti prodotti.
+                            </Typography>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                className="back-to-shop-button"
+                                onClick={() => history.push('/')}>
+                                Back to shop
+                            </Button>
+                        </div>
+                    ) : null
+                    }
+                </Container>
+            )
+        }
+
     }
 }
 
