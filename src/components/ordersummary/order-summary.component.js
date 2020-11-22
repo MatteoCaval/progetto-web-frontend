9df8a1fb328818f691d42dc7a874e7b409ed9b93
matteo cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-11-22 12:51:41 +0100

    Add form element and fix submission

--- src/components/ordersummary/order-summary.component.js
+++ src/components/ordersummary/order-summary.component.js
@@ -1,21 +1,12 @@
-import React, { useEffect, useState } from "react";
-import {
-    Button,
-    Card,
-    CardActionArea,
-    Container,
-    Grid,
-    MenuItem,
-    TextField,
-    Typography
-} from "@material-ui/core";
-import { completeOrder } from "../../redux/orders/orders.actions";
-import { clearOrderData, fetchTodayTimetable } from "../../redux/cart/cart.actions";
-import { connect } from "react-redux";
+import React, {useEffect, useState} from "react";
+import {Button, Card, CardActionArea, Container, Grid, MenuItem, TextField, Typography} from "@material-ui/core";
+import {completeOrder} from "../../redux/orders/orders.actions";
+import {clearOrderData, fetchTodayTimetable} from "../../redux/cart/cart.actions";
+import {connect} from "react-redux";
 import PaymentType from "./payment-type"
 
 import "./order-summary.style.scss"
-import { withRouter } from "react-router-dom";
+import {withRouter} from "react-router-dom";
 import HorizontalDivider from "../custom/horizontal-divider.component";
 
 const cities = [
@@ -71,167 +62,175 @@ const OrderSummaryPage = ({ user, completeOrder, total, timeSlots, fetchTodayTim
         setOrderData({ ...orderData, [name]: value })
     }
 
+    const handleSubmit = e => {
+        e.preventDefault()
+        completeOrder(orderData)
+    }
+
     return (
         <main>
-        <Typography variant="h1" className="page-title">Order summary</Typography>
+            <Typography variant="h1" className="page-title">Order summary</Typography>
             <Container maxWidth='md'>
                 <div className="order-summary-header">
                     <div className="summary-total-container">
                         <Typography variant='h2' color='textPrimary'>Total:</Typography>
                         <Typography variant='h3' color='textPrimary'>{total}€</Typography>
                     </div>
-                    <HorizontalDivider />
+                    <HorizontalDivider/>
                 </div>
-
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            id='name'
-                            name='name'
-                            value={orderData.name}
-                            variant='outlined'
-                            autoComplete='off'
-                            required
-                            fullWidth
-                            onChange={handleChange}
-                            label='Name' />
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            id='surname'
-                            name='surname'
-                            value={orderData.surname}
-                            variant='outlined'
-                            autoComplete='off'
-                            required
-                            fullWidth
-                            onChange={handleChange}
-                            label='Surname' />
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            id='address'
-                            name='address'
-                            variant='outlined'
-                            autoComplete='off'
-                            required
-                            fullWidth
-                            onChange={handleChange}
-                            label='Address' />
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            select
-                            id="city"
-                            name="city"
-                            value={orderData.city}
-                            required
-                            label="Città"
-                            fullWidth
-                            onChange={handleChange}
-                            variant="outlined">
-                            {
-                                cities.map((city, index) =>
-                                    <MenuItem key={index} value={city}>{city}</MenuItem>
-                                )
-                            }
-                        </TextField>
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            id='telephoneNumber'
-                            name='telephoneNumber'
-                            variant='outlined'
-                            autoComplete='off'
-                            required
-                            fullWidth
-                            type='tel'
-                            onChange={handleChange}
-                            label='Telephone Number' />
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        {
-                            timeSlots ? <TextField
-                                select
-                                id="timeSlot"
-                                name="timeSlot"
-                                disabled={timeSlots.length < 1}
-                                value={orderData.timeSlot}
+                <form onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id='name'
+                                name='name'
+                                value={orderData.name}
+                                variant='outlined'
+                                autoComplete='off'
                                 required
-                                label="Orario di consegna"
                                 fullWidth
                                 onChange={handleChange}
-                                children={[]}
+                                label='Name'/>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id='surname'
+                                name='surname'
+                                value={orderData.surname}
+                                variant='outlined'
+                                autoComplete='off'
+                                required
+                                fullWidth
+                                onChange={handleChange}
+                                label='Surname'/>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id='address'
+                                name='address'
+                                variant='outlined'
+                                autoComplete='off'
+                                required
+                                fullWidth
+                                onChange={handleChange}
+                                label='Address'/>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                select
+                                id="city"
+                                name="city"
+                                value={orderData.city}
+                                required
+                                label="Città"
+                                fullWidth
+                                onChange={handleChange}
                                 variant="outlined">
                                 {
-                                    timeSlots && timeSlots.map((slot, index) =>
-                                        <MenuItem key={index} value={slot}>{slot}</MenuItem>
+                                    cities.map((city, index) =>
+                                        <MenuItem key={index} value={city}>{city}</MenuItem>
                                     )
                                 }
-                            </TextField> : null
+                            </TextField>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                id='telephoneNumber'
+                                name='telephoneNumber'
+                                variant='outlined'
+                                autoComplete='off'
+                                required
+                                fullWidth
+                                type='tel'
+                                onChange={handleChange}
+                                label='Telephone Number'/>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            {
+                                timeSlots ? <TextField
+                                    select
+                                    id="timeSlot"
+                                    name="timeSlot"
+                                    disabled={timeSlots.length < 1}
+                                    value={orderData.timeSlot}
+                                    required
+                                    label="Orario di consegna"
+                                    fullWidth
+                                    onChange={handleChange}
+                                    children={[]}
+                                    variant="outlined">
+                                    {
+                                        timeSlots && timeSlots.map((slot, index) =>
+                                            <MenuItem key={index} value={slot}>{slot}</MenuItem>
+                                        )
+                                    }
+                                </TextField> : null
+                            }
+
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <Card className={cashPayment ? 'card-selected' : ''}
+                                  variant='outlined'>
+                                <CardActionArea className='payment-card'
+                                                onClick={handleCashPaymentSelect}>
+                                    <div
+                                        className='payment-card-content'>
+                                        <Typography variant='h3' color='textPrimary' className="payment-title">
+                                            Paga in contanti
+                                        </Typography>
+                                        <Typography color='textPrimary'>
+                                            Paga in contanti all'arrivo del fattorino.
+                                        </Typography>
+                                        <Typography color='textPrimary'>
+                                            Nessuna spesa aggiunta, ma ti chiediamo di preparare i contanti.
+                                        </Typography>
+                                    </div>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <Card className={cashPayment ? '' : 'card-selected'}
+                                  variant='outlined'>
+                                <CardActionArea className='payment-card'
+                                                onClick={handlePayNowSelect}>
+                                    <div
+                                        className='payment-card-content'>
+                                        <Typography variant='h3' color='textPrimary' className="payment-title">
+                                            Paga ora
+                                        </Typography>
+                                        <Typography color='textPrimary'>
+                                            La comodità di pagare subito con la tua carta.
+                                        </Typography>
+                                    </div>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+
+
+                    </Grid>
+                    <div className="summary-button-container">
+                        <Button
+                            className="place-order"
+                            variant='contained'
+                            type='submit'
+                            disabled={timeSlots.length < 1}
+                            color='secondary'>
+                            Concludi l'ordine
+                        </Button>
+                        {
+                            timeSlots.length < 1 ?
+                                <Typography variant="h3" className="no-slots-label">Al momento il negozio è chiuso. Non
+                                    è possibile effetuare nessun ordine.</Typography> : null
                         }
-
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        <Card className={cashPayment ? 'card-selected' : ''}
-                            variant='outlined'>
-                            <CardActionArea className='payment-card'
-                                onClick={handleCashPaymentSelect}>
-                                <div
-                                    className='payment-card-content'>
-                                    <Typography variant='h3' color='textPrimary' className="payment-title">
-                                        Paga in contanti
-                                </Typography>
-                                    <Typography color='textPrimary'>
-                                        Paga in contanti all'arrivo del fattorino.
-                                </Typography>
-                                    <Typography color='textPrimary'>
-                                        Nessuna spesa aggiunta, ma ti chiediamo di preparare i contanti.
-                                </Typography>
-                                </div>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
-
-                    <Grid item xs={12} sm={6}>
-                        <Card className={cashPayment ? '' : 'card-selected'}
-                            variant='outlined'>
-                            <CardActionArea className='payment-card'
-                                onClick={handlePayNowSelect}>
-                                <div
-                                    className='payment-card-content'>
-                                    <Typography variant='h3' color='textPrimary' className="payment-title">
-                                        Paga ora
-                                </Typography>
-                                    <Typography color='textPrimary'>
-                                        La comodità di pagare subito con la tua carta.
-                                </Typography>
-                                </div>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
-
-
-                </Grid>
-                <div className="summary-button-container">
-                    <Button className="place-order" variant='contained'
-                        disabled={timeSlots.length < 1}
-                        onClick={() => completeOrder(orderData)}
-                        color='secondary'>
-                        Concludi l'ordine
-                </Button>
-                    {
-                        timeSlots.length < 1 ?
-                            <Typography variant="h3" className="no-slots-label">Al momento il negozio è chiuso. Non è
-                            possibile effetuare nessun ordine.</Typography> : null
-                    }
-                </div>
+                    </div>
+                </form>
             </Container>
         </main>
     )
