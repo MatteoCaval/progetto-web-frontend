595a6ff47e200359090e351de693b4c4c8683928
matteo cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-11-22 17:41:07 +0100

    Fix get user role

--- src/components/orders/live-orders.component.js
+++ src/components/orders/live-orders.component.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import OrderList from "./orders-list.component";
-import { connect } from "react-redux";
-import { startLiveOrderUpdated, stopLiveOrderUpdated } from "../../redux/orders/orders.actions";
+import {connect} from "react-redux";
+import {startLiveOrderUpdated, stopLiveOrderUpdated} from "../../redux/orders/orders.actions";
 import UserRoles from "../../common/UserRoles";
-import { Container } from "@material-ui/core";
-import { fetchRiders } from "../../redux/admin/admin.actions";
+import {Container} from "@material-ui/core";
+import {fetchRiders} from "../../redux/admin/admin.actions";
 
-const LiveOrdersPage = ({ startLiveOrderUpdated, orders, stopLiveOrderUpdated, fetchRiders, userRole}) => {
+const LiveOrdersPage = ({ startLiveOrderUpdated, orders, stopLiveOrderUpdated, fetchRiders, userRole }) => {
 
     useEffect(() => {
         startLiveOrderUpdated()
@@ -16,7 +16,7 @@ const LiveOrdersPage = ({ startLiveOrderUpdated, orders, stopLiveOrderUpdated, f
     }, [])
 
     useEffect(() => {
-        if (userRole === UserRoles.ADMIN){
+        if (userRole === UserRoles.ADMIN) {
             fetchRiders()
         }
     }, [])
@@ -33,7 +33,7 @@ const adminFilters = ['PENDING', 'IN_DELIVERY']
 const riderFilter = ['IN_DELIVERY']
 
 const mapStateToProps = state => {
-    const userRole = state.user ? state.user.role : null
+    const userRole = (state.user && state.user.data) ? state.user.data.role : null
     return {
         orders: state.orders.realTimeOrders.filter(order => {
             return userRole === UserRoles.RIDER ? riderFilter.includes(order.state) : adminFilters.includes(order.state)
